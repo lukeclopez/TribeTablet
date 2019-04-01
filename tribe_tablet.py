e70e82ecddea0360230e4de097cc8444645a6eb4
Luke Lopez <xlucas14@gmail.com>
2019-03-31 21:15:13 -0400

    Added play page

--- tribe_tablet.py
+++ tribe_tablet.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,4 +6,12 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/play", methods=['POST'])
+def play():
+    tribe = request.form['tribe']
+    return render_template("play.html",
+                           tribe=tribe,
+                           )
 
